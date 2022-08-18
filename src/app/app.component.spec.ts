3563e02f8cde00b440dfd632ed6f6e45d80e5785
Felipe Milani <felipe.milani@talentfour.com.br>
2022-08-17 22:27:40 -0300

    app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartListItemComponent } from './components/cart-list/cart-list-item/cart-list-item.component';
+import { CartListComponent } from './components/cart-list/cart-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product-list/product/product.component';
+import { HomeComponent } from './pages/home/home.component';
+import { reducers } from './store';
+import { ShopEffects } from './store/effects';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([ShopEffects])
       ],
       declarations: [
-        AppComponent
-      ],
+        HomeComponent,
+        AppComponent,
+        HeaderComponent,
+        ProductListComponent,
+        ProductComponent,
+        CartListComponent,
+        CartListItemComponent ]
     }).compileComponents();
   });
 
@@ -24,12 +47,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('shopping-cart');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shopping-cart app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product-list/product/product.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { ShopEffects } from './store/effects';
+import { reducers } from './store';
+import { CartListComponent } from './components/cart-list/cart-list.component';
+import { CartListItemComponent } from './components/cart-list/cart-list-item/cart-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductComponent,
+    CartListComponent,
+    CartListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([ShopEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
